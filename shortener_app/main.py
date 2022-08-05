ea29277ef7f9571f4fb71df8ffb9ff72a4d2f479
deepaktripathi <tripathideepak2489@gmail.com>
2022-08-05 07:32:45 +0530

    post correct changes

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,4 +1,5 @@
 # shortener_app/main.py
+import secrets
 
 from fastapi import Depends, FastAPI, HTTPException, Request
 from pydantic import validators
@@ -38,10 +39,29 @@ def get_db():
 
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
-    if not validators.url(url.target_url):
-        raise_bad_request(message="Your provided URL is not valid")
-        db_url = crud.create_db_url(db=db, url=url)
+    # if not validators.url(url.target_url):
+    #     raise_bad_request(message="Your provided URL is not valid")
+
+    db_url = crud.create_db_url(db=db, url=url)
     return get_admin_info(db_url)
+# @app.post("/url", response_model=schemas.URLInfo)
+# def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+#     if not validators.url(url.target_url):
+#         raise_bad_request(message="Your provided URL is not valid")
+#
+#     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#     key = "".join(secrets.choice(chars) for _ in range(5))
+#     secret_key = "".join(secrets.choice(chars) for _ in range(8))
+#     db_url = models.URL(
+#         target_url=url.target_url, key=key, secret_key=secret_key
+#     )
+#     db.add(db_url)
+#     db.commit()
+#     db.refresh(db_url)
+#     db_url.url = key
+#     db_url.admin_url = secret_key
+#
+#     return db_url
 
 
 @app.get("/{url_key}")
